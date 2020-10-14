3e649685516788f851733c0da86715273a65ed2b
ktorres6 <ktorres6@jh.edu>
2020-10-13 20:40:05 -0700

    Adding bonus

--- mod6/scripts/app.js
+++ mod6/scripts/app.js
@@ -1,26 +1,56 @@
-(function () {
-'use strict';
+(function() {
+  'use strict';
 
-angular.module('LunchCheck', [])
+  angular.module('LunchCheck', [])
 
-.controller('LunchCheckController', LunchCheckController);
+    .controller('LunchCheckController', LunchCheckController);
 
-LunchCheckController.$inject = ['$scope'];
+  LunchCheckController.$inject = ['$scope'];
 
-function LunchCheckController($scope) {
-  $scope.intake = '';
-  $scope.checkIntake = function() {
-    if ($scope.intake.length === 0) {
-      $scope.message = "Please enter data first";
-    } else {
-      var meals = $scope.intake.split(',');
-      if (meals.length <= 3) {
+  function LunchCheckController($scope) {
+    // the input
+    $scope.intake = '';
+    // style object
+    $scope.styleProp = {};
+
+    // Check the meal intake input
+    $scope.checkIntake = function() {
+      // actual non-empty count
+      var actual = getNonEmpty($scope.intake);
+      // the style color
+      var color = 'green';
+      if (actual === 0) {
+        $scope.message = "Please enter data first";
+        color = 'red';
+      } else if (actual <= 3) {
         $scope.message = "Enjoy!";
+
       } else {
         $scope.message = "Too much!";
       }
+      $scope.styleProp.msg = {
+        'color': color
+      };
+      $scope.styleProp.box = {
+        'border': '1px solid ' + color
+      };
     }
   };
-};
+
+  // Count the non-empty tokens
+  function getNonEmpty(lunch) {
+    if (lunch.length === 0) {
+      return 0;
+    } else {
+      var count = 0;
+      var meals = lunch.split(',');
+      for (var i = 0; i < meals.length; i++) {
+        if (meals[i].trim().length > 0) {
+          count++;
+        }
+      }
+      return count;
+    };
+  };
 
 })();

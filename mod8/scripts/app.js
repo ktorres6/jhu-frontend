c0f32e1e6e03e0ae83a4e0365c21270318fd32dc
ktorres6 <ktorres6@jh.edu>
2020-11-10 21:37:51 -0800

    Final touches

--- mod8/scripts/app.js
+++ mod8/scripts/app.js
@@ -4,42 +4,71 @@
   angular.module('NarrowItDownApp', [])
     .controller('NarrowItDownController', NarrowItDownController)
     .service('MenuSearchService', MenuSearchService)
-    .constant('BaseUrl','https://davids-restaurant.herokuapp.com');
+    .directive('foundItems', FoundItemsDirective)
+    .constant('BaseUrl', 'https://davids-restaurant.herokuapp.com');
+
+  // FoundItems.$inject
+  function FoundItemsDirective() {
+    var ddo = {
+      templateUrl: 'foundItemTemplate.html',
+      scope: {
+        items: '<',
+        onRemove: '&'
+      },
+      controller: FoundItemsDirectiveController,
+      controllerAs: 'menu',
+      bindToController: true
+    };
+    return ddo;
+  };
+
+  function FoundItemsDirectiveController() {
+    var menu = this;
+
+    menu.empty = function() {
+      if ( !menu.items ) {
+        return false;
+      }
+      return !menu.items.length;
+    };
+  }
 
   NarrowItDownController.$inject = ['MenuSearchService'];
 
   function NarrowItDownController(MenuSearchService) {
     var menu = this;
     menu.found;
+    // search the menu for the input
     menu.search = function(searchTerm) {
       var promise = MenuSearchService.getMatchedMenuItems(searchTerm);
-      promise.then(function(response){
-        console.log(response);
+      promise.then(function(response) {
         menu.found = response;
-      }).catch(function (error) {
-        console.log("Something went wrong.");
-      })
+      }).catch(function(error) {})
     };
 
+    // remove the item with the index
+    menu.removeItem = function(index) {
+      menu.found.splice(index, 1);
+    };
   };
 
-  MenuSearchService.$inject = ['BaseUrl','$http'];
+  MenuSearchService.$inject = ['BaseUrl', '$http'];
+
   function MenuSearchService(BaseUrl, $http) {
     var service = this;
-    service.getMatchedMenuItems = function (searchTerm) {
+    service.getMatchedMenuItems = function(searchTerm) {
       // returns the list of items from the http call
       return $http({
         method: "GET",
         url: (BaseUrl + "/menu_items.json")
-      }).then(function (result) {
+      }).then(function(result) {
         var foundItems = [];
-        if ( !searchTerm ) {
+        if (!searchTerm) {
           return foundItems;
         }
         for (var item of result.data.menu_items) {
-          var desc = item.description.toLowerCase();
-          if ( desc.includes(searchTerm.toLowerCase()) ) {
-            console.log(desc);
+          if (item.description.toLowerCase()
+            .includes(searchTerm.toLowerCase())) {
             foundItems.push(item);
           }
         }
